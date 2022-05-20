3ee5387a5d776852f5c567fa80ab5e79eb5a5959
lawrenceehimhen@gmail.com <lawrenceehimhen@gmail.com>
2022-05-20 06:20:25 +0100

    I am learning to work with functional programming in C#

---
+++ FunctionalProgrammingPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Workings;
+
+namespace FunctionalProgrammingPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var discount = ComputeExample.CalculateDiscount(100, 5);
+            Console.WriteLine(discount);
+        }
+    }
+}
---
+++ Workings/ComputeExample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Workings
+{
+    public class ComputeExample
+    {
+        public static decimal CalculateDiscount(decimal amount, decimal discountRate)
+        {
+            return amount * (1 - (1 - discountRate/100));
+        }
+    }
+}
